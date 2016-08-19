9a93d9353b97de2db28ef0c722fed2f743bd107e
Laurence Rouesnel <laurence.rouesnel@cba.com.au>
2016-08-19 12:46:53 +1000

    Removed debugging statements.

--- core-macros/src/main/scala/com/rouesnel/typedsql/core/HasHiveType.scala
+++ core-macros/src/main/scala/com/rouesnel/typedsql/core/HasHiveType.scala
@@ -11,23 +11,14 @@ case class HasStructType[T](structType: StructType)
 object HasStructType {
   implicit def materialiseHasStructType[T <: ThriftStruct]: HasStructType[T] = macro impl[T]
 
-
   def impl[T](c: Context)(implicit typ: c.WeakTypeTag[T]): c.Expr[HasStructType[T]] = {
     import c.universe._
-
-
-    println(c.enclosingPosition)
-
-    println(s"RUNNING ${typ}")
-
 
     val hiveType = new ThriftHiveTypeMacro[c.type](c)
       .convertScalaToHiveType(c.weakTypeOf[T])
 
     val result =
       q"com.rouesnel.typedsql.HasStructType[${typ}](com.rouesnel.typedsql.core.HiveType.parseHiveType(${Literal(Constant(hiveType.hiveType))}).toOption.get.asInstanceOf[com.rouesnel.typedsql.core.StructType])"
-
-    println(result)
 
     c.Expr[HasStructType[T]](result)
   }
